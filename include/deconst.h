8080bfc510d544408c5836a4d9932b1f169c36e7
ricardo <12191884+Ricardo_M_Du@user.noreply.gitee.com>
2024-04-30 18:11:13 +0800

    数字宏

---
+++ include/deconst.h
@@ -0,0 +1,17 @@
+/*
+ * @Author: Ricardo
+ * @Date: 2024-04-30 15:42:28
+ * @Last Modified by: ICEY
+ * @Last Modified time: 2024-04-30 15:45:17
+ */
+#pragma once
+#define MAX_LENGTH 1024 * 2
+#define HEAD_LENGTH 2
+#define MAX_RECVQUE 10000
+#define MAX_SENDQUE 1000
+// 头部总长度
+#define HEAD_TOTAL_LEN 4
+// 消息ID的长度
+#define HEAD_ID_LEN 2
+// 消息大小的长度
+#define HEAD_DATA_LEN 2
