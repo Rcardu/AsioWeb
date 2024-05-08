aecbae533474f1eb3ec4c72388ae390445de05f5
M.Lu <djl1090523745@gmail.com>
2024-05-08 17:35:15 +0800

    服务池模型与线程池模型的使用

--- test/bsioser_t.cc
+++ test/bsioser_t.cc
@@ -2,13 +2,16 @@
  * @Author: Ricardo
  * @Date: 2024-04-23 17:58:48
  * @Last Modified by: ICEY
- * @Last Modified time: 2024-04-23 19:28:09
+ * @Last Modified time: 2024-05-08 17:14:45
  */
 
+#include "AsioIOServicePool.h"
+#include "AsioThreadPool.h"
 #include "Server.h"
 #include "boost/asio/io_context.hpp"
 #include <csignal>
 #include <iostream>
+#include <memory>
 #include <mutex>
 #include <thread>
 
@@ -16,6 +19,8 @@ bool bstop = false;
 std::condition_variable cond_quit;
 std::mutex mutex_quit;
 
+// STAR: 服务器启动宏C_SIGNAL_FLAG（服务器的退出方式）
+#ifdef C_SIGNAL_FLAG
 void sign_hander(int sig) {
   if (sig == SIGINT || sig == SIGTERM) {
     std::unique_lock<std::mutex> lock_quit(mutex_quit);
@@ -23,6 +28,7 @@ void sign_hander(int sig) {
     cond_quit.notify_one();
   }
 }
+#endif // C_SINGAL_FLAG
 
 int main() {
   try {
@@ -43,13 +49,40 @@ int main() {
 
     io_context.stop();
     net_work_thread.join();
-#endif
+#endif // C_SIGNAL_FLAG
+// STAR:服务池宏C_SERVER_POOL
+#ifdef C_SERVER_POOL
+    auto pool = ICEY::AsioIOServicePool::GetInstance();
     boost::asio::io_context io_context;
     boost::asio::signal_set signals(io_context, SIGINT, SIGTERM);
-    signals.async_wait([&io_context](auto, auto) { io_context.stop(); });
+    signals.async_wait([&io_context, pool](auto, auto) {
+      io_context.stop();
+      pool->Stop();
+    });
     ICEY::Server s(io_context, 10086);
     io_context.run();
+#endif // C_SERVER_POOL
+// STAR:线程池C_THREAD_POOL
+#ifdef C_THREAD_POOL
+    auto pool = ICEY::AsioThreadPool::GetInstance();
+    boost::asio::io_context io_context;
+    boost::asio::signal_set signals(io_context, SIGINT, SIGTERM);
+    signals.async_wait([&io_context, pool](auto, auto) {
+      io_context.stop();
+      pool->Stop();
+      std::unique_lock<std::mutex> lock(mutex_quit);
+      bstop = true;
+      cond_quit.notify_one();
+    });
+    ICEY::Server s(pool->GetIOService(), 10086);
+    {
+      std::unique_lock<std::mutex> lock(mutex_quit);
+      while (!bstop) {
+        cond_quit.wait(lock);
+      }
+    }
 
+#endif // C_THREAD_POOL 229seconds
   } catch (std::exception &e) {
     std::cerr << "Exception: " << e.what() << std::endl;
   }
